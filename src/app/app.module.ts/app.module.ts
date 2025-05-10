2ecc34063155e09eacc47b90ab946c80250500f3
tk <ktrupti222@gmail.com>
2025-05-10 16:11:04 +0530

    first commit

---
+++ src/app/app.module.ts/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from './app-material.module';
+import { AppComponent } from '../app.component';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    BrowserModule,
+    RouterModule
+    
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/hr/job-post/job-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-post',
+  imports: [],
+  templateUrl: './job-post.component.html',
+  styleUrl: './job-post.component.css'
+})
+export class JobPostComponent {
+
+}
